57509b44f181dba1c964911805be70906e355670
Gursangat <pahwagursangat2@gmail.com>
2022-07-12 01:45:09 +0530

    heroku

---
+++ test.py
@@ -0,0 +1,12 @@
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
+
+# updater = Updater('bot5521409024:AAGtt1z3ZvG-xkHlzFNFQva4MSp0d4j0chU',use_context=True)
+
+
+# def start(update: Update, context: CallbackContext):
+# 	update.message.reply_text("WELCOME")
